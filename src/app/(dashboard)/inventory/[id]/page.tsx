c6d6e5c177f486e515f2473ec90688b92ae716f1
brettzky10 <brett.linseman@gmail.com>
2024-10-15 04:54:56 -0400

    added inventory page

--- src/app/(dashboard)/inventory/[id]/page.tsx
+++ src/app/(dashboard)/inventory/[id]/page.tsx
@@ -24,3 +24,13 @@ const ProductPage = async ({
     );
 };
 export default ProductPage; */
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
