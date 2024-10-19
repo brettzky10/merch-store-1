de7037b0f9ba42e2f8c92bf37cdd9bd99d2967c9
brettzky10 <brett.linseman@gmail.com>
2024-10-19 15:28:46 -0400

    added orders to dashboard

--- src/lib/actions/dashboard/custom-products.ts
+++ src/lib/actions/dashboard/custom-products.ts
@@ -1,4 +1,4 @@
-'use server'
+/* 'use server'
 
 import prismadb from '@/lib/prismadb'
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -47,4 +47,4 @@ export async function getOrders() {
     console.error('Failed to fetch orders:', error);
     throw new Error('Failed to fetch orders');
   }
-}
+} */
